1f64f3b51e5481bc8caba5c90dbab936f959ea6b
iamPedram1 <98766992+iamPedram1@users.noreply.github.com>
2022-05-03 12:59:37 +0430

    Refactoring the code

--- script.js
+++ script.js
@@ -1,60 +1,29 @@
-// const btn = document.querySelector(".btn").addEventListener("click", submit);
-// function submit(e) {
-//   const inputEmail = document.querySelector(".input__email").value.trim();
-//   const inputPassword = document.querySelector(".input__password").value.trim();
+// Select Button and add Click Event
+const btn = document.querySelector(".btn").addEventListener(
+  "click",
+  (submit = (e) => {
+    // remove spaces from start and end of the input
+    const inputEmail = document.querySelector(".input__email").value.trim(),
+      inputPassword = document.querySelector(".input__password").value.trim();
 
-//   if (inputEmail.length < 1 || inputPassword.length < 6) return;
-//   fetch("https://dummyjson.com/users")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       const dataList = Array.from(data.users);
-//       for (let item of dataList) {
-//         const email = item.email;
-//         const password = item.password;
-//         if (inputEmail === email && inputPassword === password) {
-//           // Create Element
-//           const newDiv = document.createElement("div");
-//           const text = document.createTextNode("Logged in Succesfully");
-//           newDiv.appendChild(text);
-//           newDiv.setAttribute("class", "sucess");
-//           const container = document.querySelector(".container");
-//           container.insertBefore(newDiv, null);
-//           return;
-//         }
-//       }
-//       // Create Element
-//       const newDiv = document.createElement("div");
-//       const text = document.createTextNode(
-//         "Your Email or Password is Incorrect"
-//       );
-//       newDiv.appendChild(text);
-//       newDiv.setAttribute("class", "fail");
-//       const container = document.querySelector(".container");
-//       container.insertBefore(newDiv, null);
-//     });
-// }
+    // Checking if input has been entered correctly
+    if (inputEmail.length < 1 || inputPassword.length < 6) return;
 
-// // if (inputEmail.toLowerCase() == email && inputPassword == password) {
-// // }
+    // Fetch User informations from Api
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        const dataList = data.users;
 
-const submit = (e) => {
-  const inputEmail = document.querySelector(".input__email").value.trim();
-  const inputPassword = document.querySelector(".input__password").value.trim();
-  if (inputEmail.length < 1 || inputPassword.length < 6) return;
-  fetch("https://dummyjson.com/users")
-    .then((res) => res.json())
-    .then((data) => {
-      const dataList = Array.from(data.users);
-      const found = dataList.find((item) => {
-        return item.email == inputEmail && item.password == inputPassword;
+        // Finding User in the API
+        const found = dataList.find((item) => {
+          return item.email == inputEmail && item.password == inputPassword;
+        });
+        if (found !== undefined) {
+          alert("Logged in succesfully");
+        } else {
+          alert("Email/Password is incorrect");
+        }
       });
-      console.log(found);
-      if (found !== undefined) {
-        alert("Logged in succesfully");
-      } else {
-        alert("email/password is incorrect");
-      }
-    });
-};
-
-const btn = document.querySelector(".btn").addEventListener("click", submit);
+  })
+);
